5122425718ac6f4b53536fe9d356721da295130c
Matheus Gava <mgava@minsait.com>
2021-10-13 12:36:44 -0300

    Layout Xadrez

--- src/application/Programa.java
+++ src/application/Programa.java
@@ -1,12 +1,13 @@
 package application;
 
-import camadaTabuleiro.Tabuleiro;
+import xadrez.PartidaXadrez;
 
 public class Programa {
 
 	public static void main(String[] args) {
 		
-		Tabuleiro tabuleiro = new Tabuleiro(8, 8);
+		PartidaXadrez partidaXadrez = new PartidaXadrez();
+		UI.printTabuleiro(partidaXadrez.getPecas());
 	}
 
 }
---
+++ src/xadrez/PecaXadrez.java
@@ -0,0 +1,19 @@
+package xadrez;
+
+import camadaTabuleiro.Peca;
+import camadaTabuleiro.Tabuleiro;
+
+public class PecaXadrez extends Peca {
+
+	private Cor cor;
+
+	public PecaXadrez(Tabuleiro tabuleiro, Cor cor) {
+		super(tabuleiro);
+		this.cor = cor;
+	}
+
+	public Cor getCor() {
+		return cor;
+	}
+
+}
