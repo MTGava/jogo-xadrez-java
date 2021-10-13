50559d9977eb0f8e6f50599cb559178c682d3580
Matheus Gava <mgava@minsait.com>
2021-10-13 12:04:03 -0300

    Primeira classe: Posicao

---
+++ src/application/Programa.java
@@ -0,0 +1,14 @@
+package application;
+
+import camadaTabuleiro.Posicao;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		
+		Posicao pos = new Posicao(3, 5);
+		System.out.println(pos);
+		
+	}
+
+}
