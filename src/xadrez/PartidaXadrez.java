285c39c2dc9c3b90798bcb81272f4b513fd8e7ba
Matheus Gava <mgava@minsait.com>
2021-10-13 15:21:42 -0300

    XadrezException, PosicaoXadrez e UI para terminal

--- src/xadrez/PartidaXadrez.java
+++ src/xadrez/PartidaXadrez.java
@@ -1,6 +1,5 @@
 package xadrez;
 
-import camadaTabuleiro.Posicao;
 import camadaTabuleiro.Tabuleiro;
 import xadrez.pecas.Rei;
 import xadrez.pecas.Torre;
@@ -25,9 +24,23 @@ public class PartidaXadrez {
 		return mat;
 	}
 
+	private void posicaoNovaPeca(char coluna, int linha, PecaXadrez pecaXadrez) {
+		tabuleiro.posicaoPeca(pecaXadrez, new PosicaoXadrez(coluna, linha).toPosicao());
+	}
+
 	private void setupInicial() {
-		tabuleiro.posicaoPeca(new Torre(tabuleiro, Cor.BRANCO), new Posicao(2, 1));
-		tabuleiro.posicaoPeca(new Rei(tabuleiro, Cor.PRETO), new Posicao(0, 4));
-		tabuleiro.posicaoPeca(new Rei(tabuleiro, Cor.BRANCO), new Posicao(7, 4));
+		posicaoNovaPeca('c', 1, new Torre(tabuleiro, Cor.BRANCO));
+		posicaoNovaPeca('c', 2, new Torre(tabuleiro, Cor.BRANCO));
+		posicaoNovaPeca('d', 2, new Torre(tabuleiro, Cor.BRANCO));
+		posicaoNovaPeca('e', 2, new Torre(tabuleiro, Cor.BRANCO));
+		posicaoNovaPeca('e', 1, new Torre(tabuleiro, Cor.BRANCO));
+		posicaoNovaPeca('d', 1, new Rei(tabuleiro, Cor.BRANCO));
+
+		posicaoNovaPeca('c', 7, new Torre(tabuleiro, Cor.PRETO));
+		posicaoNovaPeca('c', 8, new Torre(tabuleiro, Cor.PRETO));
+		posicaoNovaPeca('d', 7, new Torre(tabuleiro, Cor.PRETO));
+		posicaoNovaPeca('e', 7, new Torre(tabuleiro, Cor.PRETO));
+		posicaoNovaPeca('e', 8, new Torre(tabuleiro, Cor.PRETO));
+		posicaoNovaPeca('d', 8, new Rei(tabuleiro, Cor.PRETO));
 	}
 }
